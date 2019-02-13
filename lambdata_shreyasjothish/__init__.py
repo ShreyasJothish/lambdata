7bdb568a2459286f835467164cc5764704b9159c
ShreyasJothish <shreyasjothish@gmail.com>
2019-02-12 16:54:59 -0800

    Updated the __init__.py file with correct import statements

--- lambdata_shreyasjothish/__init__.py
+++ lambdata_shreyasjothish/__init__.py
@@ -1,3 +1,7 @@
 VERSION = 0.1
 
-from usefulhacks import UsefulHacks
+#from usefulhacks import UsefulHacks
+#import usefulhacks
+#import . usefulhacks
+from . import usefulhacks
+from .usefulhacks import UsefulHacks
