4a470d8160b8157029981ae358b280cda6d8175c
ShreyasJothish <shreyasjothish@yahoo.co.in>
2019-02-13 16:10:25 -0800

    Removed warnings
    Updated the file to remove warnings based on http://pep8online.com

--- lambdata_shreyasjothish/usefulhacks.py
+++ lambdata_shreyasjothish/usefulhacks.py
@@ -4,30 +4,36 @@ Developer: Shreyas Jothish
 Description: Provides code snippets on useful coding hacks.
 """
 
+
 class UsefulHacks:
     def __init__(self):
         self.help()
 
     def help(self):
         """
-        This function returns list of function names with useful coding hacks snippets.
+        This function returns list of function names with useful coding \
+        hacks snippets.
         """
-        print('Here the code snippets assumes pandas and numpy are imported as given below')
+        print('Here the code snippets assumes pandas and numpy are imported \
+        as given below')
         print('import pandas as pd')
         print('import numpy as np')
         print('df refers to pd.DataFrame object')
         print('col refers to column name within a df')
         print('row refers to row name within a df')
-        print('------------------------------------------------------------------------------')
+        print('--------------------------------------------------------------')
         print('Function names with useful coding hacks snippets')
-        print('------------------------------------------------------------------------------')
+        print('--------------------------------------------------------------')
         print('infinity_as_na: Consider infinity as na in Pandas.')
-        print('unlimited_columns_display: Setting unlimited columns for display')
+        print('unlimited_columns_display: Setting unlimited columns for \
+        display')
         print('unlimited_rows_display: Setting unlimited rows for display')
-        print('select_numeric_columns: Selecting only numerical columns in DataFrame')
-        print('display_data_instances: Display the number of instances of data within a column')
+        print('select_numeric_columns: Selecting only numerical columns in \
+        DataFrame')
+        print('display_data_instances: Display the number of instances of \
+        data within a column')
         print('log_transformation: Log transformation of a column')
-    
+
     def infinity_as_na(self):
         """
         Code snippet for considering infinity as na in Pandas.
@@ -51,10 +57,11 @@ class UsefulHacks:
         Code snippet for selecting only numerical columns in DataFrame
         """
         print('df.select_dtypes(include=np.number)')
-    
+
     def display_data_instances(self):
         """
-        Code snippet for displaying the number of instances of data within a column
+        Code snippet for displaying the number of instances of data within a \
+        column
         """
         print('df[col].value_counts()')
 
